520978eb2e006f834df9f889ece952907aaa2ec4
naresh mahiya <nareshmahiya2017@gmail.com>
2025-04-12 00:21:39 +0530

    add style to popup

--- contentScript.js
+++ contentScript.js
@@ -16,11 +16,11 @@
             currentVideo = videoId;
             newVideoLoaded();
         }
-        else if(type === "PLAY"){
-            youtubePlayer.currentTime=value;
-        } else if(type === "DELETE"){
+        else if (type === "PLAY") {
+            youtubePlayer.currentTime = value;
+        } else if (type === "DELETE") {
             currentVideoBookmarks = currentVideoBookmarks.filter((b) => b.time != value);
-            chrome.storage.sync.set({[currentVideo]: JSON.stringify(currentVideoBookmarks)}, () => {
+            chrome.storage.sync.set({ [currentVideo]: JSON.stringify(currentVideoBookmarks) }, () => {
                 // Send back the updated bookmarks list
                 response(currentVideoBookmarks);
             });
@@ -44,31 +44,32 @@
 
 
     const newVideoLoaded = async () => {
-        const bookmarkBtnExists = document.getElementsByClassName("bookmark-btn")[0];
         currentVideoBookmarks = await fetchBookmarks();
-        if (!bookmarkBtnExists) {
-            const bookmarkBtn = document.createElement("img");//like div,p,span...create img tag element
 
-            bookmarkBtn.src = chrome.runtime.getURL("assets/bookmark.png");
-            bookmarkBtn.className = "ytp-button" + "bookmark-btn";
-            bookmarkBtn.title = "click to bookmark current timestamp";
+        const oldBtn = document.getElementsByClassName("bookmark-btn")[0];
+        if (oldBtn) oldBtn.remove();
 
-            //now take access of youtube video controles
-            youtubeLeftControles = document.getElementsByClassName("ytp-left-controls")[0];
-            youtubePlayer = document.getElementsByClassName("video-stream")[0];
+        const bookmarkBtn = document.createElement("img");//like div,p,span...create img tag element
+        bookmarkBtn.src = chrome.runtime.getURL("assets/bookmark.png");
+        bookmarkBtn.className = "ytp-button" + "bookmark-btn";
+        bookmarkBtn.title = "click to bookmark current timestamp";
 
-            //add that bookmark button
-            youtubeLeftControles.appendChild(bookmarkBtn);
-            bookmarkBtn.addEventListener("click", addNewBookmarkEventHandler);
+        //now take access of youtube video controles
+        youtubeLeftControles = document.getElementsByClassName("ytp-left-controls")[0];
+        youtubePlayer = document.getElementsByClassName("video-stream")[0];
+
+        //add that bookmark button
+        youtubeLeftControles.appendChild(bookmarkBtn);
+        bookmarkBtn.addEventListener("click", addNewBookmarkEventHandler);
 
 
-        }
+
     }
 
-    newVideoLoaded(); //it prevent us from edge case like  when page is refreshed ....
+    // newVideoLoaded(); //it prevent us from edge case like  when page is refreshed ....
     // only problem is this will cause the function to be executed twice but will be handled in if(!bookmarkBtn Exists)
 
-    const addNewBookmarkEventHandler = async() => {
+    const addNewBookmarkEventHandler = async () => {
         const currentTime = youtubePlayer.currentTime;
         currentVideoBookmarks = await fetchBookmarks(); // to make sure using latest bookmarks
 
